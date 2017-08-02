e5d1b35e2c29ad7f896f9f44ae21ce0c01097894
herman <snta2008@126.com>
2017-08-02 16:03:19 +0800

    初始化项目

---
+++ src/main/java/com/ieas/aplatform/AplatformApplication.java
@@ -0,0 +1,12 @@
+package com.ieas.aplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplatformApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ieas/aplatform/AplatformApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ieas.aplatform;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AplatformApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
